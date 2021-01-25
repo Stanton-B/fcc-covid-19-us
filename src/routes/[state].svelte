496fd0bc370bfa5837c95f25855b86a5468fded0
Stanton <stantonburton@gmail.com>
2021-01-24 20:43:58 -0600

    Got pages and components working.

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,10 +1,18 @@
+<script context="module">
+	export async function preload(page) {
+		return { state: page.params["state"] };
+	}
+</script>
+
 <script>
 	import CovidStat from "../components/CovidStat.svelte";
 	import CovidChart from "../components/CovidChart.svelte";
 	import TableContainer from "../components/TableContainer.svelte";
+
+	export let state;
 </script>
+
+<h1>The state is: {state}</h1>
 
 <CovidStat />
 <CovidChart />
-
-<TableContainer />
